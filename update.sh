e32614a3bde9637f252347b815dbce114b79fac0
Zsolt Essig <zsolteszku@gmail.com>
2015-08-25 23:22:37 +0200

    RG-4 update update.sh

--- update.sh
+++ update.sh
@@ -3,7 +3,7 @@
 git pull
 cd upload
 mkdir system/cache
-chmod 0777 system/cache/
+chmod -R 0777 system/cache/
 mkdir system/logs/
 chmod 0777 system/logs/
 mkdir system/download/
@@ -16,3 +16,4 @@ chmod 0777 image/cache/
 chmod 0777 image/catalog/
 chmod 0777 config.php
 chmod 0777 admin/config.php
+chmod -R 0777 ./
