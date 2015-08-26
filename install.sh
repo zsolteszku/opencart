991482ab8f3d8c1de7836fe9717498dda92e558d
Zsolt Essig <zsolteszku@gmail.com>
2015-08-26 18:17:38 +0200

    RG-4 rename update.sh to install.sh and fixing the installing problem

--- install.sh
+++ install.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
 
-git pull
 cd upload
+if [[ ! -f config.php || ! -f admin/config.php ]]; then
+	echo "Not correctly configured yet!"
+    rm config.php
+    rm admin/config.php
+    cp config-dist.php config.php
+    cp admin/config-dist.php admin/config.php
+else
+	echo "Correctly configured. So deleting install directory"
+	rm -rf install
+fi
 mkdir system/cache
 chmod -R 0777 system/cache/
 mkdir system/logs/
