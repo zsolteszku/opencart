affe666d0069f476222e7b090dc201018692b468
Zsolt Essig <zsolteszku@gmail.com>
2015-08-29 11:27:06 +0200

    RG-2 fully functional language_coverity script

--- scripts/language_coverity/language_coverity.py
+++ scripts/language_coverity/language_coverity.py
@@ -49,6 +49,7 @@ logger.addHandler(hdlr)
 import re
 pattern = re.compile(ur'\$_\[[\'"](.*)[\'"]\]\s*=\s*[\'"](.*)[\'"];')
 print_duplicate = False
+ignored_keys = ["heading_title", "text_edit", "text_empty", "text_success", "text_message"]
 
 def err(msg):
     logger.error(msg)
@@ -65,28 +66,74 @@ def get_keys(file):
             matches = re.match(pattern, line)
             if matches is None:
                 parsing_error += 1
-                err("Parsing error at: {}:{} in line: {}".format(file.name, linenum, line))
+                err("{}:{} >Parsing error in line: {}".format(file.name, linenum, line))
             else:
-                key = matches.group(1)
-                value = matches.group(2)
-                result[key]=value
+                key = matches.group(1).decode("utf-8").encode(sys.stdout.encoding, "ignore")
+                value = matches.group(2).decode("utf-8").encode(sys.stdout.encoding, "ignore")
+                if not value:
+                    err("{}:{} > Value is null or empty for key: {}".format(file.name, linenum, key))
+                elif key in result and result[key] != value:
+                    err("{}:{}> Duplicate key('{}', Value1='{}' Value2='{}')".format(file.name, linenum, key, result[key], value))
+                else:
+                    result[key]=value
     return result
+
+class LanguageEntity:
+
+    def __init__(self, file, value):
+        self.file = file
+        self.value = value
+
+    def __str__(self):
+        return "{}> \"{}\"".format(self.file, self.value)
 
 class LanguageValue:
 
     def __init__(self, key, file, value ):
         self.key = key
-        self.file = file
-        self.value = value
+        self.values = [LanguageEntity(file, value)]
 
-def add_to_all_dictionary(all_dic, dic):
+    def add_value(self, file, value):
+        self.values.append(LanguageEntity(file, value))
+
+    def get_prompt(self):
+        str = "There are value(s) for key(\"{}\") that already defined, you can select one of them:\n\t0. Create new value\n".format(self.key)
+        idx = 1
+        for entity in self.values:
+            str += "\t{}. {}\n".format(idx, entity)
+            idx += 1
+        return str + "Select a number: "
+
+    def contains(self, value):
+        for entity in self.values:
+            if entity.value == value:
+                return True
+        return False
+
+    def values_str(self):
+        str = "["
+        for entity in self.values:
+            str += entity.value + ", "
+        return str[:-2] + "]" 
+
+def add_to_all_dictionary(all_dic, dic, file):
     for key in dic:
+        if key in ignored_keys:
+            continue
         value = dic[key]
         if key in all_dic:
-            if value != all_dic[key]:
-                err("Found duplicate key, but value is different. Key = \"{}\" Value1 = \"{}\" Value2 = \"{}\"".format(key, value, all_dic[key]))
+            if not all_dic[key].contains(value):
+                all_dic[key].add_value(file, value)
+                #logger.warning("Duplicate key, but value is not the same: {} Values: {}".format(key, all_dic[key].values_str()))
         else:
-            all_dic[key] = value
+            all_dic[key] = LanguageValue(key, file, value)
+
+def write_pair(check_file, key, good):
+    check_file.write(u"\n$_['{}']\t\t\t= '{}';".format(key.decode(sys.stdin.encoding), good.decode(sys.stdin.encoding).encode("utf8")))
+
+def get_new_key(msg):
+    sys.stdout.write(msg)
+    return sys.stdin.readline().strip()
 
 def parse_file_pair(reference_filepath, check_filepath, add_to_all):
     global key_errors, interactive, total_reference_keys, total_check_keys, all_reference_keys, all_check_keys
@@ -94,30 +141,49 @@ def parse_file_pair(reference_filepath, check_filepath, add_to_all):
         reference_keys = get_keys(reference_file)
         check_keys = get_keys(check_file)
         if add_to_all:
-            add_to_all_dictionary(all_reference_keys, reference_keys)
-            add_to_all_dictionary(all_check_keys, check_keys)
+            add_to_all_dictionary(all_reference_keys, reference_keys, reference_filepath)
+            add_to_all_dictionary(all_check_keys, check_keys, check_filepath)
             return
     total_reference_keys+=len(reference_keys)
     total_check_keys +=len(check_keys)
-    if interactive:
-        check_file = codecs.open(check_filepath, "a", "utf-8")
-    firstError = True
-    for key in reference_keys:
-        if not key in check_keys:
-            if interactive:
-                sys.stdout.write("{} ({})> Key for ['{}'](English is: \"{}\"): ".format(check_file.name,sys.getdefaultencoding(), key, reference_keys[key]))
-                good = sys.stdin.readline().strip().decode(sys.stdin.encoding).encode("utf8")
-                if firstError:
-                    check_file.write(u"\n\n<?php\n//Missing keys with language_coverity script")
-                    firstError = False
-                check_file.write(u"\n$_['{}']        = '{}';".format(key, good))
-            else:
-                key_errors += 1
-                err("Key error. Not found key: {} in file: {}".format(key, check_file.name))
-    if interactive:
-        if not firstError:
-            check_file.write("\n?>")
-        check_file.close()
+    try:
+        if interactive:
+            check_file = codecs.open(check_filepath, "w", "utf-8")
+            check_file.seek(0)
+            check_file.write(u"//Processed with language_coverity script")
+            for key in check_keys:
+                write_pair(check_file, key, check_keys[key])
+        for key in reference_keys:
+            if not key in check_keys:
+                if interactive:
+                    good = ""
+                    if key in all_check_keys:
+                        ok = False
+                        while not ok:
+                            sys.stdout.write("{} > {}".format(check_file.name, all_check_keys[key].get_prompt()))
+                            response = sys.stdin.readline().strip()
+                            if response.isdigit():
+                                index = int(response)
+                                if index <= len(all_check_keys[key].values):
+                                    if index == 0:
+                                        good = get_new_key("Please give me the new value: ")
+                                    else:
+                                        good = all_check_keys[key].values[index-1].value
+                                    ok = True
+                                    break
+                            err("Wrong response got. Please select a number between 0 and {}!".format(len(all_check_keys[key].values)))
+                                
+                    else:
+                        good = get_new_key("{} > Key for ['{}'](English is: \"{}\"): ".format(check_file.name, key, reference_keys[key]))
+                    write_pair(check_file, key, good)
+                else:
+                    key_errors += 1
+                    err("Key error. Not found key: {} in file: {}".format(key, check_file.name))
+    except:
+        if interactive:
+            check_file.write(u"\n?>\n")
+            check_file.close()
+            raise
 
 def parse_files(add_to_all):
     if add_to_all:
@@ -126,18 +192,25 @@ def parse_files(add_to_all):
     for root, dirs, filenames in os.walk(reference_path):
         relroot = os.path.relpath(root, reference_path)
         for filename in filenames:
-            total_files += 1
+            if not add_to_all:
+                total_files += 1
             logger.debug("root: {} filename: {}".format(relroot, filename))
             if filename == "{}.php".format(reference_lang):
                 check_filename = "{}.php".format(check_lang)
             else:
                 check_filename = filename
             check_filepath = os.path.abspath(os.path.join(check_path, relroot, check_filename))
-            if os.path.exists(check_filepath):
-                parse_file_pair(os.path.join(root, filename), check_filepath, add_to_all)
-            else:
-                logger.error("Cannot find file but in the reference its exists: {}".format(check_filepath))
-                file_errors += 1
+            if not os.path.exists(check_filepath):
+                logger.error("Cannot find file but in the reference its exists: {}. Will create it!".format(check_filepath))
+                if not add_to_all:
+                    file_errors += 1
+                if not os.path.exists(os.path.dirname(check_filepath)):
+                    logger.error("Cannot find dir but in the reference its exists: {}. Will create it!".format(os.path.dirname(check_filepath)))
+                    os.makedirs(os.path.dirname(check_filepath))
+                codecs.open(check_filepath, "a", "utf-8").close()
+            parse_file_pair(os.path.join(root, filename), check_filepath, add_to_all)
+                
+                
 
 def main():
     parser = ExtendedArgumentParser(description="LanguageCoverity for OpenCart")
@@ -166,6 +239,7 @@ def main():
     total_reference_keys=0
     total_check_keys=0
     interactive = parsed.interactive
+    parse_files(True)
     parse_files(False)
     err("TotalReferenceKeys: {}\nTotalCheckKeys: {}\nTotalFiles: {}\nFileErrors: {}\nKeyErrors: {}\nParsingError: {}".format(total_reference_keys,total_check_keys,total_files, file_errors, key_errors, parsing_error))
            
